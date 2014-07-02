c0ee720cdd0ece5305067a60e219c19177e0589c
Tewodros <tedyhabtegebrial@gmail.com>
2014-07-02 16:00:22 +0300

    Modifications made: Node LearningAlgorithm asd testNode are placed in different files

--- Node.py
+++ Node.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul  1 2014
+Created on Tue Jul  2 2014
 @author: teddy
 """
 
 import numpy as np
 import scipy.io as io
 from random import randrange
-import theano.tensor as TT
+import theano
 from theano import function
+import theano.tensor as T
+from LearningAlgorithm import *
+rng = np.random
+
 class Node:
 	def __init__(self,LayerNumber,NodePos):
 		self.LayerNumber = LayerNumber
@@ -16,16 +20,17 @@ class Node:
 
 	def initLearningAlgorithm(self,AlgorithmChoice,AlgParams,InitNodeBelief,InitNodeLearnedFeatures):
 		self.AlgorithmChoice = AlgorithmChoice
-		if AlgorithmChoice == 'SparseAutoEncoder':
-			InitialWeightMatrix = InitNodeLearnedFeatures #
-			self.LearningAlgorithm = SparseAutoEncoder(AlgParams,InitNodeLearnedFeatures)
-		elif AlgorithmChoice == 'KMeansClustering':
-			#InitialCentroids = InitNodeLearnedFeatures #
-			#self.LearningAlgorithm = KMClustering(AlgParams,InitialCentroids)
-		elif AlgorithmChoice == 'EvolutionaryLearning':
-			# God Knows what to do here :D
-		elif AlgorithmChoice == 'MiscLearning':
-			# We'll write new stuff here, like combining two learning algorithms
+		if AlgorithmChoice == 'LogRegression':
+			self.AlgorithmChoice = AlgorithmChoice # Name of the Algorithm
+			self.Belief = InitNodeBelief
+			self.LearnedFeatures = InitNodeLearnedFeatures
+			#Attrbutes For the learning Algorithm Class
+			self.LearningAlgorithm = LearningAlgorithm(AlgParams)
+			self.LearningAlgorithm.D = AlgParams['D']
+			self.LearningAlgorithm.N = AlgParams['N']
+			self.LearningAlgorithm.training_steps = AlgParams['training_steps']
+			self.LearningAlgorithm.feats = AlgParams['feats']
+			self.LearningAlgorithm.w = AlgParams['w']
 		else:
 			print('make sure that you are choosing an available learning algorithm')
 			print('python is exitting')
@@ -35,10 +40,9 @@ class Node:
 		self.Input = Input
 
 	def doLearning(self, Mode):
-		self.LearningAlgorithm.runLearningAlgorithm(Mode) 
-		# Mode Differentiates Training from Testing 
-		# Mode == 1 Training: there will be update of Model/ or parameters
-		# Mode == 0 Testing: No model-updating only encoding
+		self.LearningAlgorithm.runLearningAlgorithm(Mode)
+		self.Belief = self.LearningAlgorithm.w * self.LearningAlgorithm.D[0]
+		self.LearnedFeatures = self.LearningAlgorithm.w
 '''
 Steps to train a node
 	1. Declare The Node Object with LayerNum and LayerPosition
