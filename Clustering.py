803185b2f6e9389a9d400daf77d33f8d3e35e61d
teddy <teddy@teddy.(none)>
2014-07-31 16:12:07 +0300

    Clustering theano-ed

--- Clustering.py
+++ Clustering.py
@@ -36,15 +36,15 @@ class Clustering:
         #self.mean = 10*np.random.rand(self.CENTS, self.DIMS)
         self.mean = 2*f()
         #print self.mean
-        var1 = T.dscalar('var1')
-        var2 = T.dmatrix('var2')
+        var1 = T.scalar('var1')
+        var2 = T.matrix('var2')
         var3 = T.mul
-        self.var = 0.001 * np.ones((self.CENTS, self.DIMS))
-        self.starv = np.ones((self.CENTS, 1))
-        self.belief = np.zeros((1, self.CENTS))
+        self.var = 0.001 * np.ones((self.CENTS, self.DIMS)).astype(np.float32)
+        self.starv = np.ones((self.CENTS, 1)).astype(np.float32)
+        self.belief = np.zeros((1, self.CENTS)).astype(np.float32)
 
         self.children = []
-        self.last = np.zeros((1, self.DIMS))
+        self.last = np.zeros((1, self.DIMS)).astype(np.float32)
         self.whitening = False
 
     def update_node(self, input, TRAIN):
@@ -65,7 +65,7 @@ class Clustering:
 
         # Calculate Distance
         diff = input - self.mean
-        sqdiff = np.square(diff)
+        sqdiff = np.square(diff).astype(np.float32)
         if TRAIN:
             self.train_node(diff, sqdiff)
         self.produce_belief(sqdiff)
@@ -80,7 +80,7 @@ class Clustering:
         var1 = T.matrix('var1')
         SQRT = function([var1],[T.sqrt(var1)])
         SUM = function([var1],[T.sum(var1,axis=1)])
-        euc = np.array(SQRT(SUM(sqdiff))).reshape(self.CENTS,1)
+        euc = np.array(SQRT(SUM(sqdiff))).reshape(self.CENTS,1).astype(np.float32)
         self.update_winner(euc, diff)
 
     def update_winner(self, dist, diff):
@@ -93,9 +93,9 @@ class Clustering:
         dist = dist * self.starv
 
         # Find and Update Winner
-        winner = np.argmin(dist)
+        winner = np.argmin(dist).astype(np.float32)
         self.mean[winner, :] += self.MEANRATE * diff[winner, :]
-        vdiff = np.square(diff[winner, :]) - self.var[winner, :]  # this should be updated to use sqdiff
+        vdiff = np.square(diff[winner, :]) - self.var[winner, :].astype(np.float32)  # this should be updated to use sqdiff
         self.var[winner, :] += self.VARRATE * vdiff
         self.starv *= (1.0 - self.STARVRATE)
         self.starv[winner] += self.STARVRATE
@@ -104,9 +104,9 @@ class Clustering:
         """
         Update belief state.
         """
-        sqdiff = np.asarray(sqdiff)
-        var1 = T.dmatrix('var1')
-        var2 = T.dmatrix('var2')
+        sqdiff = np.asarray(sqdiff).astype(np.float32)
+        var1 = T.matrix('var1')
+        var2 = T.matrix('var2')
         var3 = T.div_proxy(var1,var2)
         var4 = T.sum(var1,axis=1)
         MatDiv = function([var1,var2],var3)
@@ -114,15 +114,15 @@ class Clustering:
         normdist = MatSum(MatDiv(sqdiff,self.var))
         chk = (normdist == 0)
         if any(chk):
-            self.belief = np.zeros((1, self.CENTS))
+            self.belief = np.zeros((1, self.CENTS)).astype(np.float32)
             self.belief[chk] = 1.0
         else:
-            NormD = T.dvector('NormD')
+            NormD = T.vector('NormD')
             f = function([NormD], [T.div_proxy(1,NormD)])
-            normdist = np.array(f(normdist)).ravel()
-            var1 = T.dvector('var1')# Change here if error occurred
-            var2 = T.dscalar('var2')
-            var22 = T.dvector('var22')
+            normdist = np.array(f(normdist)).ravel().astype(np.float32)
+            var1 = T.vector('var1')# Change here if error occurred
+            var2 = T.scalar('var2')
+            var22 = T.vector('var22')
             var3 = T.div_proxy(var1,var2)
             var4 = T.sum(var22)
             VecDiv = function([var1,var2],[T.div_proxy(var1,var2)])
@@ -130,7 +130,7 @@ class Clustering:
             #print(np.array(VecSum(normdist)))
             #print(np.array(normdist).shape)
             #exit(0)
-            self.belief = np.array(VecDiv(normdist,np.array(VecSum(normdist))[0])).reshape(1,self.CENTS)
+            self.belief = np.array(VecDiv(normdist,np.array(VecSum(normdist))[0])).reshape(1,self.CENTS).astype(np.float32)
 
     def init_whitening(self, mn=[], st=[], tr=[]):
         """
